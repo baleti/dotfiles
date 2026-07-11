3cc90365e5a3ce7994cd52652e3d4a862ac51a01
baleti <baleti3266@gmail.com>
2026-07-11 19:07:36 +0100

    add gitq and don't pull entire git history in any
    main branch is for 'deployment' hosts, development mostly happens on instances pulling from qemu-claude branch

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -2,5 +2,9 @@
 
 (package! git-branch-off
   :recipe (:host github :repo "baleti/git-branch-off"
-           :branch "main"
-           :depth full))
+           :branch "main"))
+
+(package! gitq
+  :recipe (:host github :repo "baleti/gitq"
+           :files ("integrations/emacs/*.el")
+           :branch "main"))
