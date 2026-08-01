9a1419cbd342a5432399a32e3dab893c6a813efc
baleti <baleti3266@gmail.com>
2026-08-01 16:42:56 +0000

    Scaffold Hyprland config as Lua modules

---
+++ .config/hypr/environment.lua
@@ -0,0 +1,31 @@
+-----------------------------
+---- ENVIRONMENT VARIABLES ---
+-----------------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("AQ_DRM_DEVICES",
+    "/home/user1/.config/hypr/Intel-integrated-GPU-DRI-card:" ..
+    "/home/user1/.config/hypr/nvidia-DRI-card:" ..
+    "/home/user1/.config/hypr/displaylink-DRI-card")
+
+
+-------------------
+---- PERMISSIONS --
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
+-- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
+-- for security reasons
+
+-- hl.config({
+--   ecosystem = {
+--     enforce_permissions = true,
+--   },
+-- })
+
+-- hl.permission({ binary = "/usr/(bin|local/bin)/grim", type = "screencopy", mode = "allow" })
+-- hl.permission({ binary = "/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", type = "screencopy", mode = "allow" })
+-- hl.permission({ binary = "/usr/(bin|local/bin)/hyprpm", type = "plugin", mode = "allow" })
