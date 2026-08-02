6aad0e55ac46bb1173162b0ccebd01230a6cab55
baleti <baleti3266@gmail.com>
2026-08-02 17:12:59 +0000

    Fix Signal window class match in apps.lua

--- .config/hypr/apps.lua
+++ .config/hypr/apps.lua
@@ -11,7 +11,7 @@ return {
     { key = "2", slug = "element",     class = "Element",                      cmd = "element-desktop" },
     { key = "3", slug = "thunderbird", class = "org.mozilla.Thunderbird",      cmd = "thunderbird" },
     { key = "4", slug = "gimp",        class = "gimp",                         cmd = "gimp-3.2" },
-    { key = "5", slug = "signal",      class = "signal",                       cmd = "flatpak run org.signal.Signal" },
+    { key = "5", slug = "signal",      class = "org.signal.Signal",            cmd = "flatpak run org.signal.Signal" },
     { key = "6", slug = "inkscape",    class = "org.inkscape.Inkscape",        cmd = "inkscape" },
     { key = "7", slug = "recoll",      class = "recoll",                       cmd = "recoll" },
 }
