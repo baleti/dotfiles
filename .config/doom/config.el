cc69c1005cae79f53826ca7a3df800a55acaee2c
baleti <baleti3266@gmail.com>
2026-08-16 16:21:02 +0100

    extra space in timestamps seemed more convenient
    in terms of positioning relative to cursor

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -250,12 +250,12 @@ runs after this hook otherwise wins."
 (defun insert-custom-timestamp-with-date ()
   "Insert the current date and time in the format: [YYYY-MM-DD Day HH:MM]."
   (interactive)
-  (insert (format-time-string "[%Y-%m-%d %a %H:%M]")))
+  (insert (format-time-string " [%Y-%m-%d %a %H:%M]")))
 
 (defun insert-custom-timestamp ()
   "Insert the current date and time in the format: =HH:MM=]."
   (interactive)
-  (insert (format-time-string "=%H:%M=")))
+  (insert (format-time-string " =%H:%M=")))
 
 (map! :leader
       :desc "Insert custom timestamp with date"
